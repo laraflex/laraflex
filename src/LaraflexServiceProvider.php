fcb481c6e832062ba5ff54af85cc1f27fb18a676
Dimas Vidal <dimasvidal@yahoo.com.br>
2019-12-04 15:47:43 -0300

    Atualização de Componentes

--- src/LaraflexServiceProvider.php
+++ src/LaraflexServiceProvider.php
@@ -23,24 +23,17 @@ class LaraflexServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadViewsFrom(__DIR__.'/Resources/Views/Components', 'laraflex');
-        $this->loadViewsFrom(__DIR__.'/Resources/Views/Components/scriptjs', 'laraflexscript');
+        $this->loadViewsFrom(__DIR__.'/Resources/Components', 'laraflex');
+        $this->loadViewsFrom(__DIR__.'/Resources/Components/scriptjs', 'laraflexscript');
         //publishes resources
         $this->publishes([__DIR__.'/Resources/lang' => base_path('resources/lang'),]);
-        $this->publishes([__DIR__.'/Resources/lang/pt' => base_path('resources/lang/pt'),]);
         $this->publishes([__DIR__.'/Resources/Views' => base_path('resources/views'),]);
-        $this->publishes([__DIR__.'/Resources/Views/auth' => base_path('resources/views/auth'),]);
-        $this->publishes([__DIR__.'/Resources/Views/auth/passwords' => base_path('resources/views/auth/passwords'),]);
-        $this->publishes([__DIR__.'/Resources/Views/layouts' => base_path('resources/views/layouts'),]);
-        $this->publishes([__DIR__.'/Resources/Views/layouts/functional' => base_path('resources/views/layouts/functional'),]);
-        $this->publishes([__DIR__.'/Resources/Views/layouts/section' => base_path('resources/views/layouts/section'),]);
         //publishes View Composers, View Presenters and ResourceMediators
         $this->publishes([__DIR__.'/App/ViewComposers' => base_path('App/ViewComposers'),]);
         $this->publishes([__DIR__.'/App/ViewPresenters' => base_path('App/ViewPresenters'),]);
         $this->publishes([__DIR__.'/App/ResourceMediators' => base_path('App/ResourceMediators'),]);
         //publishes Command and Stubs
         $this->publishes([__DIR__.'/App/Console/Commands' => base_path('App/Console/Commands'),]);
-        $this->publishes([__DIR__.'/App/Console/Commands/Stubs' => base_path('App/Console/Commands/Stubs'),]);
         //publishes public files
         $this->publishes([__DIR__.'/public/images/app' => base_path('public/images/app'),]);
         $this->publishes([__DIR__.'/public/images/icons' => base_path('public/images/icons'),]);
