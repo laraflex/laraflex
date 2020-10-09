c2dfeb9b29a631cf40b2f3a6ccb2063411637891
Dimas Vidal <dimasvidal@yahoo.com.br>
2020-10-09 10:30:56 -0300

    Atualização 09-10-2020

--- src/Resources/Views/layouts/default.blade.php
+++ src/Resources/Views/layouts/default.blade.php
@@ -6,10 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no maximum-scale=1, user-scalable=no">
     <meta name="google" content="notranslate">
     @yield('meta')
-    @php
-    $linkRel = $util->toRoute("images/favicon.png");
-    @endphp
-    <link rel="icon" type="image/png" sizes="16x16" href="{{$linkRel}}">
+    <link rel="icon" sizes="16x16" href="favicon.png">
+    <link rel="shortcut icon" type="image/png" sizes="16x16" href="favicon.png">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href="{{$util->toRoute('css/contentFlex.css')}}" rel="stylesheet">
     @yield('head')
@@ -26,7 +24,8 @@
 @yield('content')
 @yield('footer')
 <!-- JavaScript ------------------------------------- -->
-<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+<!--script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script-->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.js" integrity="sha256-DrT5NfxfbHvMHux31Lkhxg42LY6of8TaYyK50jnxRnM=" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 @yield('scriptjs')
