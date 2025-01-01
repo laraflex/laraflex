033749bcb50415b279fa1befdb6ab3117cd47192
dimasvidal <dimasvidal@yahoo.com.br>
2025-01-01 16:54:25 -0300

    Complementação do commit anterior

--- src/Resources/Views/layouts/default.blade.php
+++ src/Resources/Views/layouts/default.blade.php
@@ -1,3 +1,6 @@
+@php
+    $bootstrapCss = $defaultConfig['bootstrapcss'];
+@endphp]
 <!doctype html>
 <html lang="pt-br">
 <head>
@@ -9,7 +12,8 @@
     @yield('meta')
     <link rel="icon" sizes="16x16" href="favicon.png">
     <link rel="shortcut icon" type="image/png" sizes="16x16" href="favicon.png">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!--Configuração Bootstrap arquivo config-->
+    <link rel="stylesheet" href= {!! $bootstrapCss !!}>
     <link href="{{$util->toRoute('css/contentFlex.css')}}" rel="stylesheet">
     @yield('head')
 <style type="text/css">
@@ -25,10 +29,16 @@
 @yield('content')
 @yield('footer')
 <!-- JavaScript ------------------------------------- -->
-<!--script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script-->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.js" integrity="sha256-DrT5NfxfbHvMHux31Lkhxg42LY6of8TaYyK50jnxRnM=" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+@php
+    $jquery = $defaultConfig['jquery'];
+    $pooperJs = $defaultConfig['pooperjs'];
+    $bootstrapJs = $defaultConfig['bootstrapjs'];
+@endphp
+<!-- JavaScript ------------------------------------- -->
+<script src= {!! $jquery !!}></script>
+<script src= {!! $pooperJs !!}></script>
+<script src= {!! $bootstrapJs !!}></script>
+
 @yield('scriptjs')
 </body>
 </html>
